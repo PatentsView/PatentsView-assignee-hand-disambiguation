cbc18193a8f789681b5916a0db4ae89c11438ffe
SiddharthEngineer <54515876+SiddharthEngineer@users.noreply.github.com>
2023-08-30 12:41:25 -0700

    Updating assignee

--- assignee.py
+++ assignee.py
@@ -1,8 +1,24 @@
 from sqlalchemy import *
 from dotenv import dotenv_values
 import pandas as pd
+import numpy as np
 import os
-import pickle
+
+"""
+Establish MySQL connection with environmental variables
+
+Returns
+-------
+connection : a PyMySQL connection object
+"""
+def establish_connection():
+    config = dotenv_values(".env")
+    user = config['user']
+    pswd = config['password']
+    hostname = config['hostname']
+    dbname = config['dbname']
+    connection = create_engine(f"mysql+pymysql://{user}:{pswd}@{hostname}/{dbname}?charset=utf8mb4")
+    return connection
 
 """
 Parameters
@@ -33,12 +49,93 @@ def assignee_data(mention_id: str, connection):
     split = mention_id.split("-")
     patent_id = split[0][2:]
     sequence = split[1]
-
     # Running query on algorithms_assignee_labeling view
     query = f"SELECT * FROM algorithms_assignee_labeling.assignee WHERE patent_id='{patent_id}' and assignee_sequence='{sequence}'"
     result = connection.execute(text(query)).fetchall()
     df = pd.DataFrame(result)
     return df
+    # TODO - integrate expand_rows()
+
+"""
+Parameters
+----------
+row: pandas dataframe row
+
+Returns
+-------
+Row with merged cells for CPC subgroup and inventors such that each mention_id
+    has one embedded row for each CPC category and one embedded for for each inventor
+"""
+def expand_row(row):
+    return None # TODO
+
+"""
+Parameters
+----------
+
+Pulls a random sample of assignee mention_id's from AWS and saves it as an output CSV file
+"""
+def sample(size=10000):
+    np.random.seed(0)
+    disamb = pd.read_csv(
+        "https://s3.amazonaws.com/data.patentsview.org/download/g_persistent_assignee.tsv.zip",
+        dtype=str,
+        sep="\t",
+        compression="zip")
+
+    disamb["mention_id"] = "US" + disamb["patent_id"] + "-" + disamb["assignee_sequence"]
+    disamb_20220929 = disamb.set_index("mention_id")["disamb_assignee_id_20220929"]
+    disamb_20220929 = disamb_20220929.dropna()
+    mention_ids = disamb_20220929.index
+
+    sample = np.random.choice(mention_ids, size=10000, replace=False)
+    pd.Series(sample).to_csv("data/sample.csv")
+
+"""
+Parameters
+----------
+Sample: list[str]
+    List of mention IDs (`mention_id` field values)
+Output Path: str
+    Path to CSV file for saving populated data
+Connection: sqlalchemy.engine.base.Connection
+    Connection using sqlalchemy to the PV database.
+
+Output
+-------
+Saves data to `output_path` which is a dataframe with one row for every element in `sample`
+Each row has all the attributes in assignee_data()
+"""
+def populate_sample(sample_path, output_path, connection):
+    # Load sample data and determine which are previously populated
+    sample = pd.read_csv(sample_path)['0'].tolist()
+    prev_df = pd.read_csv(output_path) if os.path.exists(output_path) else pd.DataFrame()
+    df_list = [prev_df]
+    temp_list = []
+    populated = ["US" + str(row.patent_id) + "-" + str(row.assignee_sequence) for index, row in prev_df.iterrows()]
+
+    for mention_id in sample:
+        if mention_id in populated:
+            continue
+
+        # Populate mention_id and save data
+        temp_df = assignee_data(mention_id, connection)
+        df_list.append(temp_df)
+        populated.append(mention_id)
+
+        # Output messages
+        percent = str(round(100 * len(populated) / len(sample), 1)) + "%"
+        print(percent, "- created row for", mention_id)
+
+        # Store dataframe intermitently
+        if len(populated) % 20 == 0:
+            temp_df = pd.concat(temp_list, axis=0, ignore_index=True)
+            df_list.append(temp_df)
+            pd.concat(df_list, axis=0, ignore_index=True).to_csv(output_path)
+            temp_list = []
+        
+    # Save final dataframe
+    df = pd.concat(df_list, axis=0, ignore_index=True).to_csv(output_path)
 
 """
 Parameters
@@ -60,58 +157,18 @@ def disambiguated_assignees_data(assignee_disambiguation_IDs: list[str], connect
     result = connection.execute(text(query)).fetchall()
     df = pd.DataFrame(result)
     return df
-
-"""
-Parameters
-----------
-Sample: list[str]
-    List of mention IDs (`mention_id` field values)
-Output Path: str
-    Path to CSV file for saving populated data
-Connection: sqlalchemy.engine.base.Connection
-    Connection using sqlalchemy to the PV database.
-
-Output
--------
-Saves data to `output_path` which is a dataframe with one row for every element in `sample`
-Each row has all the attributes in assignee_data()
-"""
-def populate_sample(sample, output_path, connection):
-    # Load sample data and previously populated
-    total = len(sample)
-    prev_df = pd.read_csv(output_path) if os.path.exists(output_path) else pd.DataFrame()
-    df_list = [prev_df]
-    populated = ["US" + str(row.patent_id) + "-" + str(row.assignee_sequence) for index, row in prev_df.iterrows()]
-
-    for mention_id in sample:
-        # Populate mention_id and save data
-        temp_df = assignee_data(mention_id, connection)
-        df_list.append(temp_df)
-        populated.append(mention_id)
-
-        # Output messages
-        percent = str(round(100 * len(populated) / total, 1)) + "%"
-        print(percent, "- created row for", mention_id)
-
-        # Store dataframe intermitently
-        if len(populated) % 20 == 0:
-            df = pd.concat(df_list, axis=0, ignore_index=True)
-            df.to_csv('output.csv')
-        
-    # Save final dataframe
-    df = pd.concat(df_list, axis=0, ignore_index=True)
-    df.to_csv('output.csv')
+    # TODO - move mention_id row to top
+    # TODO - integrate expand_rows()
 
 def main():
-    # Establishing SQL connection
-    config = dotenv_values(".env")
-    engine = create_engine(f"mysql+pymysql://{config['user']}:{config['password']}@{config['hostname']}/{config['dbname']}?charset=utf8mb4")
-
+    engine = establish_connection()
+    
     # Calling first method
     with engine.connect() as connection:
-        sample = pd.read_csv('data/sample.csv')['0'].tolist()
-        populate_sample(sample, 'data/output.csv', connection)
-        # ids = ['8314bfc2-8005-4202-ae98-63c90a4e245e']
+        # Generates data for sample
+        
+        populate_sample('data/sample.csv', 'data/samples_with_data.csv', connection)
+        # ids = ['717e8394-c25d-4ea6-b444-09d6036a4cde']
         # df = disambiguated_assignees_data(ids, connection)
         # df.to_csv('output.csv')
 
