78b51e98da2fda226fc3f2f3007d4110170a878b
SiddharthEngineer <54515876+SiddharthEngineer@users.noreply.github.com>
2023-10-03 13:23:38 -0400

    Added methods and reorganized code

--- assignee.py
+++ assignee.py
@@ -9,14 +9,28 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 from tqdm import tqdm
 
 """
+Dictionary for converting assignee type into it's actual meaning
+"""
+ASSIGNEE_TYPE_DICT = {
+    1: "Unassigned",
+    2: "United States company or corporation",
+    3: "Foreign company or corporation",
+    4: "United States individual",
+    5: "Foreign individual",
+    6: "U.S. Federal government",
+    7: "Foreign government",
+    8: "U.S. county government",
+    9: "U.S. state government"
+}
+
+
+"""
 Establish MySQL connection with environmental variables
 
 Returns
 -------
 connection : a PyMySQL connection object
 """
-
-
 def establish_connection():
     config = dotenv_values(".env")
     user = config['user']
@@ -55,13 +69,12 @@ Pandas Dataframe, with columns for (the non-disambiguated version of):
     - Inventor locations
     - CPC Subgroups
 """
-
-
 def assignee_data(mention_id: str, patent_only: bool, connection):
     # Getting patent information
     split = mention_id.split("-")
     patent_id = split[0][2:]
     sequence = split[1]
+
     # Running query on algorithms_assignee_labeling view
     if patent_only:
         query = f"SELECT patent_id, assignee_sequence, organization, name_first, name_last, assignee_type, title, patent_date,\
@@ -71,7 +84,10 @@ def assignee_data(mention_id: str, patent_only: bool, connection):
         query = (f"SELECT * FROM algorithms_assignee_labeling.assignee WHERE patent_id='{patent_id}' and "
                  f"assignee_sequence='{sequence}'")
     result = connection.execute(text(query)).fetchall()
+
+    # Return pandas df
     df = pd.DataFrame(result).drop_duplicates()
+    df['assignee_type'] = df['assignee_type'].map(ASSIGNEE_TYPE_DICT)
     return df
 
 
@@ -83,9 +99,8 @@ size : int
 
 Pulls a random sample of assignee mention_id's from AWS and saves it as an output CSV file
 """
-
-
-def sample_mentions(size=10000):
+def sample_mentions(size=10000, output_path="data/01 - sample.txt"):
+    # Save seed and load in data
     np.random.seed(0)
     disamb = pd.read_csv(
         "g_persistent_assignee.tsv.zip",
@@ -93,37 +108,33 @@ def sample_mentions(size=10000):
         sep="\t",
         compression="zip")
 
+    # Clean data
     disamb["mention_id"] = "US" + disamb["patent_id"] + "-" + disamb["assignee_sequence"]
     disamb_20220929 = disamb.set_index("mention_id")["disamb_assignee_id_20230629"]
     disamb_20220929 = disamb_20220929.dropna()
     mention_ids = disamb_20220929.index
 
+    # Save output
     samples = np.random.choice(mention_ids, size=size, replace=False)
-    np.savetxt("data/01 - sample.txt", samples, fmt="%s")
-    # with open(, "w") as outfile:
-    #     outfile.write("\n".join(str(item) for item in samples.tolist()))
-
-    # pd.Series(samples).to_csv("data/sample.csv")
+    np.savetxt(output_path, samples, fmt="%s")
 
 
 """
 Parameters
 ----------
+connection : sqlalchemy.engine.base.Connection
+    Connection using sqlalchemy to the PV database.
 sample_path : str
     Path to CSV file containing a list of mention IDs (`mention_id` field values)
 output_path : str
     Path to CSV file for saving populated data. This method both reads and writes for intermitent progress
-connection : sqlalchemy.engine.base.Connection
-    Connection using sqlalchemy to the PV database.
 
 Output
--------
+------
 Saves data to `output_path` which is a dataframe with one row for every element in `sample`
 Each row has all the attributes in assignee_data()
 """
-
-
-def populate_sample(sample_path, output_path, connection):
+def populate_sample(connection, sample_path="01 - sample.txt", output_path="02 - sample_with_data.csv"):
     # Load sample data and determine which are previously populated
     sample = np.loadtxt(sample_path, dtype=str)
     prev_df = pd.read_csv(output_path) if os.path.exists(output_path) else pd.DataFrame()
@@ -154,6 +165,42 @@ def populate_sample(sample_path, output_path, connection):
 """
 Parameters
 ----------
+n (int) : Number of hand labelers
+sample_path (str) : path to CSV file containing samples with data
+output_path (str) : folder to save n different dataframes
+
+Output
+------
+Folder with n different files, each an equally sized partition of sample_path
+"""
+def segment_sample(n=3, sample_path="data/02 - sample_with_data.csv", output_folder="data/03 - segmented samples/"):
+
+    # Load input data and create output folder
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    samples = pd.read_csv(sample_path, index_col=0)
+    sample_count = len(samples.index)
+
+    # Values to increment
+    increment_size = sample_count // n
+    start = 0
+    end = increment_size
+
+    # Loop through each hand labeler
+    for i in range(n):
+        end = end if i in range(sample_count % n) else end - 1 # Handle uneven distribution
+
+        # Save each data partition
+        output_path = os.path.join(output_folder, str(i) + " - hand_labeler_file.csv")
+        samples.loc[start:end].to_csv(output_path)
+
+        # Increment values for next iteration
+        start = end + 1
+        end = start + increment_size
+
+"""
+Parameters
+----------
 ws : openpyxl worksheet
     Worksheet for appending rows from 'df' and merging like cells
 index_start : int
@@ -168,21 +215,23 @@ Returns
 ws : openpyxl worksheet
     Updated worksheet with all rows from `df` to `ws`
 """
-
-
 def merge_cells(ws, index_start, index_end, num_columns):
+    # Loop over columns
     for i in range(1, num_columns + 1):
+
+        # Loop over rows in pre-defined range
         start = index_start
         end = start
         for j in range(index_start, index_end + 1):
-            if j == start:
+
+            if j == start: # Initialize the previous value at the start of the section
                 prev_value = ws.cell(column=i, row=j).value
             elif ws.cell(column=i, row=j).value == prev_value:
-                if j == index_end:
+                if j == index_end: # Consecutive cells with same value need to merge if end of section
                     ws.merge_cells(start_row=start, end_row=j, start_column=i, end_column=i)
-                else:
+                else: # Otherwise, consecutive cells with same value need to check the next cell before merging
                     end += 1
-            elif start != end:
+            elif start != end: # Merge prior sequence once a different value is in place
                 ws.merge_cells(start_row=start, end_row=end, start_column=i, end_column=i)
                 start = j + 1
                 end = j + 1
@@ -214,8 +263,6 @@ Specifically, rows should be of the form `assignee_data(mention_id)` for each me
 the disambiguated assignee ID in the provided list.
 Implementing merged cells to increase readability
 """
-
-
 def disambiguated_assignees_data(assignee_disambiguation_IDs: list[str], output_path: str, connection):
     id_list = '("' + '","'.join(assignee_disambiguation_IDs) + '")'
     query = f"SELECT * FROM algorithms_assignee_labeling.assignee a WHERE a.disambiguated_assignee_id IN {id_list}"
@@ -252,12 +299,4 @@ def main():
 
 
 if __name__ == "__main__":
-    import time
-
-    start = time.time()
-    # sample_mentions(750)
-    engine = establish_connection()
-    with engine.connect() as connection:
-        populate_sample(sample_path="data/01 - sample.txt", output_path="data/02 - sample_with_data.csv",
-                        connection=connection)
-    # print("Step took :{duration}".format(duration=time.time() - start))
+    main()
