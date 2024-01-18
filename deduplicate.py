65f43a4499d1a2c8539a90026eca0c531ed83722
SiddharthEngineer <54515876+SiddharthEngineer@users.noreply.github.com>
2024-01-18 13:54:43 -0600

    Setting up resolution to cluster overlap issues

--- deduplicate.py
+++ deduplicate.py
@@ -3,15 +3,14 @@ import sys
 import os
 
 # Read user input
-INPUT_DIR = "data/06 - compare/"
-OUTPUT_DIR = "data/07 - evaluation/"
-MENTION_ID = sys.argv[1]
-labeler1 = sys.argv[2]
-labeler2 = sys.argv[3]
+INPUT_DIR = "/Users/sengineer/Library/CloudStorage/OneDrive-AIR/Week 2 Dropbox/"
+OUTPUT_DIR = "data/08 - deduplication/"
+MENTION_ID_1 = sys.argv[1]
+MENTION_ID_2 = sys.argv[2]
 
 # Load in user data
-f1_path = os.path.join(INPUT_DIR, MENTION_ID + "-" + labeler1 + ".csv")
-f2_path = os.path.join(INPUT_DIR, MENTION_ID + "-" + labeler2 + ".csv")
+f1_path = os.path.join(INPUT_DIR, MENTION_ID_1 + ".csv")
+f2_path = os.path.join(INPUT_DIR, MENTION_ID_2 + ".csv")
 file1 = pd.read_csv(f1_path, index_col=0, dtype={'patent_id': str, 'assignee_state': object})
 file2 = pd.read_csv(f2_path, index_col=0, dtype={'patent_id': str, 'assignee_state': object})
 print("Done reading the files.")
@@ -33,12 +32,23 @@ file1_patents = set(file1.patent_id) - set(file2.patent_id)
 file2_patents = set(file2.patent_id) - set(file1.patent_id)
 file1_diff = file1[file1.patent_id.isin(file1_patents)].copy()
 file2_diff = file2[file2.patent_id.isin(file2_patents)].copy()
-file1_diff["Labeler"] = labeler1
-file2_diff["Labeler"] = labeler2
+
+# Indicate origin file
+file1_diff["Origin"] = "1"
+file2_diff["Origin"] = "2"
+file1_diff["Mention ID"] = MENTION_ID_1
+file2_diff["Mention ID"] = MENTION_ID_2
 merged = pd.concat([file1_diff, file2_diff])
 print("Done merging the files.")
 
 # Filter rows where both 'File1' and 'File2' are False
-output_path = os.path.join(OUTPUT_DIR, MENTION_ID + "-difference.csv")
+output_path = os.path.join(OUTPUT_DIR, MENTION_ID_1 + "_" + MENTION_ID_2 + ".csv")
 merged.to_csv(output_path, index=False)
 print("Successfully saved the output file.")
+
+# Code used for when two mention IDs belong to different clusters
+# intersection_patents = set(file1.patent_id).intersection(set(file2.patent_id))
+# print(intersection_patents)
+# intersection = file1[file1.patent_id.isin(intersection_patents)].copy()
+# output_path = os.path.join(OUTPUT_DIR, MENTION_ID_1 + "_" + MENTION_ID_2 + ".csv")
+# intersection.to_csv(output_path, index=False)
